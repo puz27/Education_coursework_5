879e78651a1f54539055d8d98655e6e08f0a9328
Nikolay <n.avramenko87@gmail.com>
2023-04-21 20:21:03 +0300

    add request create tables in data base

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 import requests
 
+
 url_head_hunter = "https://api.hh.ru/employers"
 params = {
-    "text": "YES Холдинг",
+    "text": "газпром",
     "only_with_vacancies": "true",
     "per_page": 10,
     "page": 1,
@@ -10,16 +11,39 @@ params = {
 
 response = requests.get(url_head_hunter, params=params)
 print(response)
-all_firms = response.json()
+all_firms = response.json()["items"]
 print(all_firms)
-# for firm in all_firms:
-#
-#     print("Фирма:", firm["name"])
-#     print(firm["id"])
-#     firm_id = firm["id"]
-#     print("Количество открытых вакансий", firm["open_vacancies"])
-#     print(firm["vacancies_url"])
-#     firm_id_url = firm["vacancies_url"]
+
+for firm in all_firms:
+
+    print("Фирма:", firm["name"])
+    print(firm["id"])
+    firm_id = firm["id"]
+    print("Количество открытых вакансий", firm["open_vacancies"])
+    print(firm["vacancies_url"])
+    firm_id_url = firm["vacancies_url"]
+
+    vacancy_id_response = requests.get(firm_id_url)
+    vacancies = response.json()["items"]
+    print(vacancies)
+    for vacancy in vacancies:
+        print(vacancy["name"])
+        print(vacancy["id"])
+        print(vacancy["vacancies_url"])
+        url_head_hunter = vacancy["vacancies_url"]
+
+        response_vacancy = requests.get(url_head_hunter)
+        print(response_vacancy.reason)
+        vacancies_in_firm = response_vacancy.json()["items"]
+
+        for vacancy in vacancies_in_firm:
+            print(vacancy["id"])
+            print(vacancy["name"])
+            print(vacancy["salary"])
+            print(vacancy["address"])
+            print("---------------------------------------")
+
+
 
     # vacancy_id_response = requests.get(firm_id_url)
     # vacancies = response.json()["items"]
--- src/DBManager.py
+++ src/DBManager.py
@@ -1,3 +1,6 @@
+import psycopg2
+
+
 class DBManager:
 
     def get_companies_and_vacancies(self):
@@ -14,3 +17,49 @@ class DBManager:
 
     def get_vacancies_with_keyword(self):
         pass
+
+def send_query_create_tables() -> None:
+    """
+    Send SQL queries to Data Base north on localhost
+    :param table: Name of table in database.
+    :param args: List of data for adding.
+    :param user: User name connection.
+    :param password: Password user for connection.
+    """
+    connection = psycopg2.connect(
+        host="localhost",
+        database="course",
+        user="postgres",
+        password="123456"
+    )
+    try:
+        with connection:
+            with connection.cursor() as cursor:
+
+                query_company = """
+                CREATE TABLE companies(
+                customer_id VARCHAR(20) PRIMARY KEY,
+                company_name VARCHAR(50) NOT NULL, 
+	            contact_name VARCHAR(30));
+	            """
+
+                query_vacancies = """
+                CREATE TABLE vacancies (
+                customer_id VARCHAR(20) PRIMARY KEY, 
+                company_name VARCHAR(50) NOT NULL, 
+                contact_name VARCHAR(30));
+                """
+
+                cursor.execute(query_company)
+                cursor.execute(query_vacancies)
+                connection.commit()
+                print(f"Операция над таблицей прошла успешно.")
+    except psycopg2.Error:
+        print("Ошибка с запросом.")
+    finally:
+        connection.close()
+
+
+send_query_create_tables()
+
+
