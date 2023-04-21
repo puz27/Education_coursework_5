59ad7c8921f86cd1936f96e6415a8be5177da238
Nikolay <n.avramenko87@gmail.com>
2023-04-21 18:35:12 +0300

    add class RequestManager/DBManager

---
+++ src/DBManager.py
@@ -0,0 +1,16 @@
+class DBManager:
+
+    def get_companies_and_vacancies(self):
+        pass
+
+    def get_all_vacancies(self):
+        pass
+
+    def get_avg_salary(self):
+        pass
+
+    def get_vacancies_with_higher_salary(self):
+        pass
+
+    def get_vacancies_with_keyword(self):
+        pass
