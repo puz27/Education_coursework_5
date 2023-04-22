cafed76957787b8fd409e4e24e2807b157222f3a
Nikolay <n.avramenko87@gmail.com>
2023-04-22 18:50:20 +0300

    add request for vacancies

--- main.py
+++ main.py
@@ -10,64 +10,41 @@ params = {
 }
 
 response = requests.get(url_head_hunter, params=params)
-print(response)
 all_firms = response.json()["items"]
-print(all_firms)
+
+companies_data = []
+vacancies_data = []
 
 for firm in all_firms:
-
-    print("Фирма:", firm["name"])
-    print(firm["id"])
+    # print("Фирма:", firm["name"])
+    # print("ID:", firm["id"])
     firm_id = firm["id"]
-    print("Количество открытых вакансий", firm["open_vacancies"])
-    print(firm["vacancies_url"])
+    # print("Количество открытых вакансий", firm["open_vacancies"])
+    # print(firm["vacancies_url"])
+
+    # Url for vacancies if company
     firm_id_url = firm["vacancies_url"]
 
-    vacancy_id_response = requests.get(firm_id_url)
-    vacancies = response.json()["items"]
-    print(vacancies)
-    for vacancy in vacancies:
-        print(vacancy["name"])
-        print(vacancy["id"])
-        print(vacancy["vacancies_url"])
-        url_head_hunter = vacancy["vacancies_url"]
+    # Add all companies (ID, Description, counts of vacancies) to list
+    one_company = (firm["id"], firm["name"], firm["open_vacancies"])
+    companies_data.append(one_company)
 
-        response_vacancy = requests.get(url_head_hunter)
-        print(response_vacancy.reason)
-        vacancies_in_firm = response_vacancy.json()["items"]
+    # Get information about vacancies in company
+    vacancy_response = requests.get(firm_id_url)
+    vacancies_of_company = vacancy_response.json()["items"]
 
-        for vacancy in vacancies_in_firm:
-            print(vacancy["id"])
-            print(vacancy["name"])
-            print(vacancy["salary"])
-            print(vacancy["address"])
-            print("---------------------------------------")
+    for vacancy in vacancies_of_company:
+        # print(vacancy["id"])
+        # print("ID Фирмы:", firm["id"])
+        # print("Название вакансии", vacancy["name"])
+        # print(vacancy["salary"])
+        # print(vacancy["address"])
+        #
+        print("---------------------------------------")
 
+        # Add all vacancies of company  to list
+        one_vacancy = (vacancy["id"], firm["id"], vacancy["name"], vacancy["salary"], vacancy["address"])
+        companies_data.append(one_vacancy)
 
-
-    # vacancy_id_response = requests.get(firm_id_url)
-    # vacancies = response.json()["items"]
-    #
-    # print("\nИнформация по вакансиям предприятия")
-    # for vacancy in vacancies:
-    #     print(vacancy["id"])
-    #     print(vacancy["name"])
-    #     print(vacancy["url"])
-
-    #print("------------------------------------------------")
-
-
-
-    # url_head_hunter2 = "https://api.hh.ru/vacancies/"
-    # params2 = {
-    #     "employer_id": firm_id,
-    #     "per_page": 10,
-    #     "page": 10,
-    # }
-    #
-    # response2 = requests.get(url_head_hunter2, params=params2)
-    # z = response2.json()
-    # print(z)
-
-
-
+    print(companies_data)
+    print(vacancies_data)
